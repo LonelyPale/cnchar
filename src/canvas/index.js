8e26a2553031e8a1ad7143e9df27ed2d682f1685
theajack <theajack@qq.com>
2019-09-29 00:14:44 +0800

    reset

--- src/canvas/index.js
+++
@@ -1,4 +0,0 @@
-
-import main from './util'
-
-export default main
--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
-import main from './main'
-main();
+import cnchar from './main/index'
+import './plugin/order'
+import './plugin/trad'
+import './plugin/poly'
+console.log('1',cnchar)
+export default cnchar;
 
+// var cnchar = require('./main/index')
+// var order = require('./plugin/order')
+// var trad = require('./plugin/trad')
+// var poly = require('./plugin/poly')
+// cnchar.use(order,trad,poly)
+// console.log(cnchar.stroke("一个",'order'))
+// console.log(cnchar.stroke('長城','count','order','name'))
+// module.exports = cnchar
 
+// import b from './b'
+// // var b = require('./b')
+// console.log(b);
