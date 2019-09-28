53ef02148c3bf8879a221ab59133c885b51c814d
theajack <theajack@qq.com>
2019-09-28 23:46:37 +0800

    init: first commit

--- helper/tool.js
+++
@@ -1,9 +0,0 @@
-let fs = require('fs');
-module.exports = {
-    write:function (file,txt,cb){
-        fs.writeFile(file, txt, 'utf8', (err) => {
-            if (err) throw err;
-            if(cb)cb();
-        });
-    }
-}
